4458807d57a7dd2d58ca1b7d52b397a0d403f283
William <victorwccv@gmail.com>
2024-05-26 00:59:53 -0500

    navbar done

--- client/src/components/navbarComp/navbar.jsx
+++ client/src/components/navbarComp/navbar.jsx
@@ -1,8 +1,26 @@
-
+import { Link } from "react-router-dom";
+import "./navbar.scss";
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <nav className="navbar">
+      <div className="container">
+        <div className="nav">
+          <h1>
+            <Link><span className="logo">VWCV</span> Bank</Link>
+          </h1>
+          <div className="links">
+            <Link className="link ">Home</Link>
+            <Link className="link ">Products</Link>
+            <Link className="link ">Suport</Link>
+          </div>
+        </div>
+        <div className="button-links">
+          <Link className="btn-link">Open Your Account</Link>
+          <Link className="link">Online Banking</Link>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
