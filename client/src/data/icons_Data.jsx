48afbe58de771053ba2eb3b29159e9dd4c5e0e5e
William <victorwccv@gmail.com>
2024-05-28 01:22:04 -0500

    online banking front done

--- client/src/data/icons_Data.jsx
+++ client/src/data/icons_Data.jsx
@@ -1,9 +1,9 @@
-import { FaRegCreditCard } from "react-icons/fa";
+import { FaRegCreditCard, FaShieldAlt } from "react-icons/fa";
 import { HiOutlineBanknotes } from "react-icons/hi2";
 import { LiaPiggyBankSolid } from "react-icons/lia";
 import { BsPersonBadge } from "react-icons/bs";
 import { RiSecurePaymentFill } from "react-icons/ri";
-import { RiHandCoinLine } from "react-icons/ri";
+import { RiHandCoinLine, RiShieldUserFill } from "react-icons/ri";
 
 
 const icons = {
@@ -13,6 +13,7 @@ const icons = {
   user: <BsPersonBadge />,
   secure: <RiSecurePaymentFill />,
   service: <RiHandCoinLine />,
+  secure: <RiShieldUserFill />,
 }
 
 export default icons
--- client/src/pages/onlineBanking_Page/onlineBanking.jsx
+++ client/src/pages/onlineBanking_Page/onlineBanking.jsx
@@ -1,10 +1,56 @@
-import Footer from "../../components/footer_Comp/footer"
+import "./onlineBanking.scss";
+
 
 function OnlineBanking() {
   return (
-    <div>OnlineBanking
+    <div className="online-banking">
+      <div className="welcome-container">
+        <h1> Online Banking </h1>
+        <p>
+          Manage your finances with ease and security. Log in to access your
+          accounts, make transactions, and more.
+        </p>
+        <ul>
+          <li>Secure and convenient access to your accounts</li>
+          <li>24/7 availability for all your banking needs</li>
+          <li>Quick transfers and bill payments</li>
+          <li>Real-time transaction alerts</li>
+        </ul>
+      </div>
+
+
+      <div className="form-container">
+        <h2 className="text-neutral-600 text-4xl font-bold text-center mb-16">Access to your Account</h2>
+        <form
+          className="flex flex-col gap-10"
+          // onSubmit={handleSubmit}
+        >
+          {/* Input fields */}
+          <input
+            type="email"
+            placeholder="Correo electrónico"
+            id="email"
+            className="bg-gray-100 p-3 rounded-lg outline-none"
+            // onChange={handleChange}
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            id="password"
+            className="bg-gray-100 p-3 rounded-lg outline-none"
+            // onChange={handleChange}
+          />
+          {/* Submit button */}
+          <button
+            // disabled={loading}
+            className="bg-green-500 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+          >
+            {"Access Account"}
+          </button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default OnlineBanking
+export default OnlineBanking;
