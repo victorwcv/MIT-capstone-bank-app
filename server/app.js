468a3edc661c2add172121d9620fa5410d95de9a
William <victorwccv@gmail.com>
2024-05-29 18:45:00 -0500

    error handler

--- server/app.js
+++ server/app.js
@@ -30,4 +30,12 @@ app.listen(_LOCAL_PORT, () => {
 app.use('/api/user', userRoute);
 app.use('/api/auth', authRoute);
 
-
+app.use((err, req, res, next)=> {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  })
+})
