957b5e5014241999bbcddb16401c36a6d5cc4190
William <victorwccv@gmail.com>
2024-06-03 12:06:27 -0500

    add redux-persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,10 +4,13 @@ import { RouterProvider } from "react-router-dom";
 import Router from "./router/routers.jsx";
 import { Provider } from "react-redux";
 import "./index.css";
-import { store } from "./store/store.js";
+import { persistor, store } from "./store/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store} >
-    <RouterProvider router={Router} />
+  <Provider store={store}>
+    <PersistGate persistor={persistor} loading={null}>
+      <RouterProvider router={Router} />
+    </PersistGate>
   </Provider>
 );
--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,12 +1,26 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import userReducer from "./slices/userSlice.js";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({user: userReducer});
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);  
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer
-  },
+  reducer: 
+    persistedReducer
+  ,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false, // Required by Redux Toolkit to work with Electron and Immer
     }),
 });
+
+export const persistor = persistStore(store);
