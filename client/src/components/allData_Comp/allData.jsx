33eb0acd977bbdabeea20c2cd5a40c81b0e20e84
William <victorwccv@gmail.com>
2024-06-05 14:18:19 -0500

    improve allData feature

--- client/src/components/allData_Comp/allData.jsx
+++ client/src/components/allData_Comp/allData.jsx
@@ -26,22 +26,30 @@ function AllData() {
 
   return (
     <>
-      <div className="flex justify-center p-4 items-center gap-10">
+      <div className="flex justify-center my-10 items-center gap-10">
         <button
           onClick={handleClick}
-          className="px-6 py-3 w-40 rounded-full text-white bg-[var(--secondary-color)]"
+          className="px-6 py-3 w-40 rounded-full text-white bg-[var(--secondary-color)] active:scale-105"
         >
           Show All Data
         </button>
         <button
           onClick={() => setUsersData(null)}
-          className="px-6 py-3 w-40 rounded-full text-white bg-[var(--secondary-color)]"
+          className="px-6 py-3 w-40 rounded-full text-white bg-[var(--secondary-color)] active:scale-105"
         >
           Clear
         </button>
       </div>
-      <div className={` flex flex-col mx-5 space-y-2 overflow-auto`}>
-        <table className="w-full shadow-md">
+      <div className={`flex flex-col mx-5 space-y-2 overflow-auto`}>
+        <h2
+          colSpan="6"
+          className={`${usersData !== null ? "hidden" : ""} p-3 text-center`}
+        >
+          No Data to Show
+        </h2>
+        <table
+          className={`${usersData === null ? "hidden" : ""} w-full shadow-md`}
+        >
           <thead className="bg-[#EBECF0] dark:bg-dark-eval-2">
             <tr className="text-center font-medium text-gray-700">
               <th className="py-3">ID</th>
@@ -53,20 +61,11 @@ function AllData() {
             </tr>
           </thead>
           <tbody className="text-left divide-y divide-gray-200 dark:divide-dark-eval-2">
-            <tr>
-              <td
-                colSpan="6"
-                className={`${
-                  usersData !== null ? "hidden" : ""
-                } p-3 text-center`}
-              >
-                No hay datos
-              </td>
-            </tr>
+            <tr></tr>
             {usersData &&
               usersData.map((user, index) => {
                 return (
-                  <tr key={index}>
+                  <tr key={index} className="bg-white cursor-pointer hover:bg-yellow-300 ">
                     <td className="p-3">{index + 1}</td>
                     <td className="p-3">{user.username}</td>
                     <td className="p-3">{user.email}</td>
--- client/src/pages/adminPanel_Page/adminPanel.jsx
+++ client/src/pages/adminPanel_Page/adminPanel.jsx
@@ -3,39 +3,68 @@ import { useState } from "react";
 import CreateNewAdmin from "../../components/createNewAdmin_Comp/createNewAdmin";
 import Footer from "../../components/footer_Comp/footer";
 
+const adminOptions = [
+  {
+    title: "All Data",
+    id: "all-data",
+  },
+  {
+    title: "Create New Admin",
+    id: "new-admin",
+  },
+  {
+    title: "User Details",
+    id: "user-details",
+  },
+  {
+    title: "Delete Users",
+    id: "delete-users",
+  },
+];
+
 function AdminPanel() {
   const [panelPage, SetPanelPge] = useState("all-data");
+
   console.log("rendering!");
+
   const handleClick = (e) => {
     e.preventDefault();
     SetPanelPge(e.target.id);
-  }
+  };
   return (
     <>
-    <section className="flex flex-col min-h-[calc(100vh-65px)]">
-      <h1 className="text-center text-4xl font-bold my-10">Admin Panel</h1>
-      <div className="container mx-auto grid grid-cols-4 gap-3 p-3 h-[700px]">
-        <div className="bg-neutral-100">
-          <button onClick={handleClick} id="all-data" className="block w-full text-center text-xl py-6 hover:bg-neutral-200">
-            All Data
-          </button>
-          <button onClick={handleClick} id="create" className="block w-full text-center text-xl py-6 hover:bg-neutral-200">
-            Create New Admin
-          </button>
-          <button onClick={handleClick} id="user-details" className="block w-full text-center text-xl py-6 hover:bg-neutral-200">
-            User Details
-          </button>
-          <button onClick={handleClick} id="delete users" className="block w-full text-center text-xl py-6 hover:bg-neutral-200">
-            Delete Users
-          </button>
+      <section className="flex flex-col min-h-[calc(100vh-65px)]">
+        <h1 className="text-center text-4xl font-bold my-10">Admin Panel</h1>
+        <div className="container mx-auto grid grid-cols-4 gap-3 p-3 h-[700px]">
+          <div className="bg-neutral-100">
+            {adminOptions.map((option, index) => {
+              return (
+                <button
+                  key={index}
+                  onClick={handleClick}
+                  id={option.id}
+                  className={`${
+                    panelPage === option.id ? "bg-neutral-200" : ""
+                  } block w-full text-center text-xl py-6 hover:bg-neutral-200`}
+                >
+                  {option.title}
+                </button>
+              );
+            })}
+          </div>
+
+          <div className="bg-neutral-200 col-span-3">
+            {panelPage === "all-data" ? (
+              <AllData />
+            ) : panelPage === "create" ? (
+              <CreateNewAdmin />
+            ) : (
+              ""
+            )}
+          </div>
         </div>
-        <div className="bg-neutral-100 col-span-3">
-          {panelPage === "all-data" ? <AllData />:
-          panelPage==='create'? <CreateNewAdmin/>:''}
-        </div>
-      </div>
-    </section>
-    <Footer />
+      </section>
+      <Footer />
     </>
   );
 }
