5f648033f1a030d004beaad4122bacef1239bb3a
William <victorwccv@gmail.com>
2024-06-07 18:55:34 -0500

    added deposit route

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { test } from "../controllers/user.controller.js";
+import { deposit, test } from "../controllers/user.controller.js";
+import { cookieJwtAuth } from "../middlewares/cookieJWTAuth.js";
 
 const router = express.Router();
 
 router.get('/', test);
+router.post('/transactions/deposit', cookieJwtAuth, deposit);
 
 export default router;
