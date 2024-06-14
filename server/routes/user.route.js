01974361654d346a6ef014c8da03fbb434a8c675
William <victorwccv@gmail.com>
2024-06-13 21:04:57 -0500

    feat: add route to create new bank account to user

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deposit, test, userData, withdrawal } from "../controllers/user.controller.js";
+import { deposit, test, userData, withdrawal, newAccount } from "../controllers/user.controller.js";
 import { cookieJwtAuth } from "../middlewares/cookieJWTAuth.js";
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get('/', test);
 router.get('/transactions/user-data', cookieJwtAuth, userData)
 router.patch('/transactions/deposit', cookieJwtAuth, deposit);
 router.patch('/transactions/withdrawal', cookieJwtAuth, withdrawal);
+router.put('/transactions/new-account', cookieJwtAuth, newAccount);
 
 export default router;
