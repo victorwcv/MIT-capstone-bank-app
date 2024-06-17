8a6d7970abfdf86e84a7f7d9fd32db9cee0e0f69
William <victorwccv@gmail.com>
2024-06-17 18:48:19 -0500

    getting balance of each account of user

--- client/src/components/transactions/BankTransfer.jsx
+++ client/src/components/transactions/BankTransfer.jsx
@@ -3,10 +3,20 @@ import { useData } from "../../hooks/useData";
 import { useUserBalance } from "../../hooks/useUserBalance";
 import * as Yup from "yup";
 import getCurrentDateTime from "../../utils/dates";
+import { useEffect, useState } from "react";
 
 function BankTransfer() {
   const { data } = useData();
   const userBalance = useUserBalance();
+  const [accountBalances, setAccountBalances] = useState({});
+
+  useEffect(() => {
+    const balances = {};
+    data.bankAccounts?.forEach((acc) => {
+      balances[acc.bankAccountNumber] = acc.accountBalance;
+    });
+    setAccountBalances(balances);
+  }, [data]);
 
   console.log("Rendering Bank Transfer!");
   return (
@@ -30,11 +40,15 @@ function BankTransfer() {
           }}
           validate={(values) => {
             const errors = {};
+            const originAccountBalance = accountBalances[values.originAccount] || 0;
             if (
               values.originAccount === values.destinationAccount &&
               values.originAccount
             ) {
               errors.destinationAccount = "Cannot transfer to same account";
+            }
+            if (values.amount && values.amount > originAccountBalance) {
+              errors.amount = `Amount exceeds available balance of the account`;
             }
 
             return errors;
@@ -53,30 +67,7 @@ function BankTransfer() {
         >
           {(formik) => (
             <Form>
-              <div className="flex flex-col gap-2 mb-6 text-lg">
-                <label htmlFor="originAccount">Choose Origin Account</label>
-                <Field
-                  as="select"
-                  id="originAccount"
-                  name="originAccount"
-                  className="px-4 py-1 outline-none text-right"
-                >
-                  <option value="">Select an Account</option>
-                  {data.bankAccounts?.map((acc, index) => {
-                    return (
-                      <option key={index} value={acc.bankAccountNumber}>
-                        {`$ ${acc.accountBalance} / ${acc.bankAccountNumber}`}
-                      </option>
-                    );
-                  })}
-                </Field>
-                <ErrorMessage
-                  name="originAccount"
-                  component="div"
-                  className="text-red-500"
-                />
-              </div>
-              <div className="flex justify-around mb-6 text-lg">
+              <div className="flex justify-around my-6 text-lg">
                 <div className="flex gap-4">
                   <Field type="radio" name="typeAccount" id="own" value="own" />
                   <label htmlFor="own">To own Account</label>
@@ -91,43 +82,67 @@ function BankTransfer() {
                   <label htmlFor="other">To other Account</label>
                 </div>
               </div>
-              <div className="flex flex-col gap-2 mb-4 text-lg">
-                {formik.values.typeAccount === "own" ? (
-                  <>
-                    <label htmlFor="destinationAccount">
-                      Choose Destination Account
-                    </label>
-                    <Field
-                      as="select"
-                      id="destinationAccount"
-                      name="destinationAccount"
-                      className="px-4 py-1 outline-none text-right"
-                    >
-                      <option>Select an Account</option>
-                      {data.bankAccounts?.map((acc, index) => {
-                        return (
-                          <option key={index} value={acc.bankAccountNumber}>
-                            {`$ ${acc.accountBalance} / ${acc.bankAccountNumber}`}
-                          </option>
-                        );
-                      })}
-                    </Field>
-                  </>
-                ) : (
-                  <>
-                    <label htmlFor="destinationAccount">
-                      Enter the destination account number
-                    </label>
-                    <Field
-                      type="text"
-                      id="destinationAccount"
-                      name="destinationAccount"
-                      className="px-4 py-1 outline-none text-right"
-                    />
-                  </>
-                )}
-                <div className="text-red-500">
-                  <ErrorMessage name="destinationAccount" />
+              <div className="flex  gap-12">
+                <div className="flex flex-col gap-2 text-lg flex-1">
+                  <label htmlFor="originAccount">Choose Origin Account</label>
+                  <Field
+                    as="select"
+                    id="originAccount"
+                    name="originAccount"
+                    className="px-4 py-1 outline-none text-right"
+                  >
+                    <option value="">Select an Account</option>
+                    {data.bankAccounts?.map((acc, index) => {
+                      return (
+                        <option key={index} value={acc.bankAccountNumber}>
+                          {`$ ${acc.accountBalance} / ${acc.bankAccountNumber}`}
+                        </option>
+                      );
+                    })}
+                  </Field>
+
+                  <div className="text-red-500 text-sm h-3 text-end">
+                    <ErrorMessage name="originAccount" />
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2 text-lg flex-1">
+                  {formik.values.typeAccount === "own" ? (
+                    <>
+                      <label htmlFor="destinationAccount">
+                        Choose Destination Account
+                      </label>
+                      <Field
+                        as="select"
+                        id="destinationAccount"
+                        name="destinationAccount"
+                        className="px-4 py-1 outline-none text-right"
+                      >
+                        <option>Select an Account</option>
+                        {data.bankAccounts?.map((acc, index) => {
+                          return (
+                            <option key={index} value={acc.bankAccountNumber}>
+                              {`$ ${acc.accountBalance} / ${acc.bankAccountNumber}`}
+                            </option>
+                          );
+                        })}
+                      </Field>
+                    </>
+                  ) : (
+                    <>
+                      <label htmlFor="destinationAccount">
+                        Enter the destination account number
+                      </label>
+                      <Field
+                        type="text"
+                        id="destinationAccount"
+                        name="destinationAccount"
+                        className="px-4 py-1 outline-none text-right"
+                      />
+                    </>
+                  )}
+                  <div className="text-red-500  text-sm h-3 text-end">
+                    <ErrorMessage name="destinationAccount" />
+                  </div>
                 </div>
               </div>
               <div className="flex gap-12">
@@ -141,7 +156,7 @@ function BankTransfer() {
                     name="amount"
                     className="px-4 py-1 outline-none text-right"
                   />
-                  <div className="text-red-500 mt-1 text-sm font-light h-3">
+                  <div className="text-red-500 text-sm h-3">
                     <ErrorMessage name="amount" />
                   </div>
                 </div>
@@ -156,7 +171,7 @@ function BankTransfer() {
                     className="px-4 py-1 outline-none text-right"
                     readOnly
                   />
-                  <div className="text-red-500 mt-1 text-sm font-light h-3">
+                  <div className="text-red-500 text-sm h-3 text-end">
                     <ErrorMessage name="transactionDate" />
                   </div>
                 </div>
@@ -171,7 +186,9 @@ function BankTransfer() {
                   name="description"
                   className="w-full h-20 px-4 py-1 outline-none rounded-lg resize-none"
                 />
-                <ErrorMessage name="description" />
+                <div className="text-red-500 text-sm h-3 text-end">
+                  <ErrorMessage name="description" />
+                </div>
               </div>
               <div className="flex justify-center">
                 <button
