30f22b46c220ebd44e1b9d67ee1ed541416e640f
William <victorwccv@gmail.com>
2024-06-17 20:37:23 -0500

    fix: bug on return value when there is no data

--- client/src/hooks/useUserBalance.js
+++ client/src/hooks/useUserBalance.js
@@ -3,6 +3,8 @@ import { useSelector } from "react-redux";
 export const useUserBalance = () => {
   const { data } = useSelector((state) => state.userData);
 
+  if (!data) return 0;
+
   const balance = data?.bankAccounts
     ?.map((account) => account.accountBalance)
     .reduce((prev, act) => prev + act, 0) || 0;
