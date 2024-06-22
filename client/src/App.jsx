bab939e666782f0babd69b111cb88c8e2c4300f7
William <victorwccv@gmail.com>
2024-06-21 21:56:47 -0500

    feat: add condition for not showing footer in the admin panel

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/navbar_Comp/Navbar";
 import Footer from "./components/footer_Comp/Footer";
 import Disclaimer from "./components/Disclaimer";
 
 
 function App() {
+  const location = useLocation();
   return (
     <div className="relative">
       <Navbar />
       <Outlet />
-      <Footer />
+      {location.pathname !== "/admin-panel" && <Footer />}
       <Disclaimer />
     </div>
   );
