e1b324c7328b15931c812378633c911e50a02477
William <victorwccv@gmail.com>
2024-06-26 18:24:35 -0500

    feat: add public route to fix bug with browser navigation arrows

---
+++ client/src/auth/PublicRoute.jsx
@@ -0,0 +1,11 @@
+import {  Navigate, Outlet } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+
+function PublicRoute() {
+  const {currentUser} = useSelector(state => state.user);
+  
+  return currentUser ? <Navigate to="/" /> : <Outlet />
+}
+
+export default PublicRoute
