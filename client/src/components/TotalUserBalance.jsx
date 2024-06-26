c9f5eb256e110cbf6f25901da6bb004fe85f2d67
William <victorwccv@gmail.com>
2024-06-26 18:27:28 -0500

    chore: change prop name for better understanding

--- client/src/components/TotalUserBalance.jsx
+++ client/src/components/TotalUserBalance.jsx
@@ -1,11 +1,11 @@
 import { totalUserBalance } from "../utils/totalUserBalance";
 
-function TotalUserBalance({ data }) {
+function TotalUserBalance({ userAccounts }) {
   return (
     <>
       <div className="mb-6 mx-12 font-medium flex justify-between text-xl border-b-2">
         <h3>Current balance:</h3>
-        <p>$ {totalUserBalance(data)}</p>
+        <p>$ {totalUserBalance(userAccounts)}</p>
       </div>
     </>
   );
