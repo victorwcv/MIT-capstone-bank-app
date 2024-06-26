2445329352b8300391eb1ec444791b7eb04eaf7f
William <victorwccv@gmail.com>
2024-06-26 18:22:27 -0500

    shows footer in all paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/navbar_Comp/Navbar";
 import Footer from "./components/footer_Comp/Footer";
 import Disclaimer from "./components/Disclaimer";
 
-
 function App() {
-  const location = useLocation();
   return (
     <div className="relative">
       <Navbar />
       <Outlet />
-      {location.pathname !== "/admin-panel" && <Footer />}
+      <Footer />
       <Disclaimer />
     </div>
   );
