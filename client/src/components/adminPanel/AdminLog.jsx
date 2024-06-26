0a6938d9c0cfe83e3bb52342c12035459b207949
William <victorwccv@gmail.com>
2024-06-26 18:29:03 -0500

    feat: new feat component file created

---
+++ client/src/components/adminPanel/AdminLog.jsx
@@ -0,0 +1,10 @@
+
+function AdminLog() {
+  return (
+    <div className="text-3xl text-neutral-500 text-center font-bold mb-10">
+      Comming soon...
+    </div>
+  )
+}
+
+export default AdminLog
