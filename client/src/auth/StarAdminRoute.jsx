9fa54723723b92bb53ac4483350b691937c4b0f0
William <victorwccv@gmail.com>
2024-06-26 18:26:05 -0500

    delete file

--- client/src/auth/StarAdminRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import { Navigate, Outlet } from "react-router-dom";
-
-function StarAdminRoute() {
-  const { administering } = useSelector((state) => state.admin);
-  return  administering ?  <Outlet /> : <Navigate to={"/admin-panel"} />;
-}
-
-export default StarAdminRoute;
