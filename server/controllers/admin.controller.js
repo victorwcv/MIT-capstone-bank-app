de629ccb8fe581c16b96d8dcd4f09defbc97cc17
William <victorwccv@gmail.com>
2024-06-26 20:24:50 -0500

    add createAdmin and searchUser controller

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
 
 export const allData = async (req, res, next) => {
   // console.log(req.user);
@@ -11,5 +12,48 @@ export const allData = async (req, res, next) => {
     res.json(users);
   } catch (error) {
     next(errorHandler(500, "Error getting users from DB"));
+  }
+};
+
+export const createNewAdmin = async (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return next(errorHandler(403, "You are not authorized"));
+  }
+  const {username,email,password,} = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newAdmin = new User({
+    username,
+    email,
+    password: hashedPassword,
+    role: "admin",
+  });
+  try {
+    console.log(newAdmin);
+    await newAdmin.save();
+    res.status(201).json({
+      status: "success",
+      message: "Admin created successfully",
+    });
+    console.log('Admin created successfully');
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export const searchUser = async (req, res, next) => {
+  if (req.user.role !== "admin") {
+    return next(errorHandler(403, "You are not authorized"));
+  }
+  const email = req.params.email;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) {
+      return next(errorHandler(404, "User not found"));
+    }
+    console.log('user found');
+    const { password, ...data } = user._doc;
+    res.status(200).json(data);
+  } catch (error) {
+    next(errorHandler(500, "Error getting user from DB"));
   }
 };
