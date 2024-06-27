9d04eec129b3b33b57b0e5266ee1915f9b2369de
William <victorwccv@gmail.com>
2024-06-26 20:28:00 -0500

    add routes

--- server/routes/admin.route.js
+++ server/routes/admin.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { allData } from '../controllers/admin.controller.js';
 import { cookieJwtAuth } from '../middlewares/cookieJWTAuth.js';
+import { allData,  createNewAdmin , searchUser} from '../controllers/admin.controller.js';
 
 const router = express.Router();
 
 router.get('/all-data',cookieJwtAuth, allData);
+router.post('/create-new-admin',cookieJwtAuth, createNewAdmin);
+router.get('/search-user/:email',cookieJwtAuth, searchUser);
 
 export default router
