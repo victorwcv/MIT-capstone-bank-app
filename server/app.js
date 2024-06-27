abe40ea83f619f77331a771aa4751425bd38a5fb
William <victorwccv@gmail.com>
2024-06-26 20:35:28 -0500

    changes for  deployment

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ mongoose
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.CLIENT_URL ?? "http://localhost:3000",
   credentials: true,
 };
 
@@ -35,7 +35,7 @@ app.use(express.json());
 const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 app.use("/api/auth", authRoute);
