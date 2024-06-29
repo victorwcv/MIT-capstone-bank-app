fe310a88f04abbb1d3b1b97185d45add7b359bcf
William <victorwccv@gmail.com>
2024-06-28 23:07:18 -0500

    fix: bug in prop name

--- client/src/components/TotalUserBalance.jsx
+++ client/src/components/TotalUserBalance.jsx
@@ -1,11 +1,11 @@
 import { totalUserBalance } from "../utils/totalUserBalance";
 
-function TotalUserBalance({ userAccounts }) {
+function TotalUserBalance({ data }) {
   return (
     <>
       <div className="mb-6 mx-12 font-medium flex justify-between text-xl border-b-2">
         <h3>Current balance:</h3>
-        <p>$ {totalUserBalance(userAccounts)}</p>
+        <p>$ {totalUserBalance(data)}</p>
       </div>
     </>
   );
