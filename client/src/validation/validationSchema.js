9e61f12878e8faaf051009ecfa2b4ee9e3cdf830
William <victorwccv@gmail.com>
2024-06-30 22:25:53 -0500

    add new validations

--- client/src/validation/validationSchema.js
+++ client/src/validation/validationSchema.js
@@ -6,14 +6,28 @@ const validationSchema = Yup.object().shape({
     .min(5, "Username must be at least 5 characters long")
     .max(20, "Username cannot be longer than 15 characters"),
   email: Yup.string()
-    .email("Invalid email address")
-    .required("Email is required"),
+    .required("Email is required")
+    .email("Invalid email address"),
   password: Yup.string()
-    .max(20, "Must be 20 characters or less")
-    .required("Required"),
+    .required("Required")
+    .max(20, "Must be 20 characters or less"),
   confirmPassword: Yup.string()
-    .oneOf([Yup.ref("password"), null], "Passwords must match")
-    .required("Required"),
+    .required("Required")
+    .oneOf([Yup.ref("password"), null], "Passwords must match"),
+  adress: Yup.string()
+    .required("Adress is required")
+    .min(5, "Adress must be at least 5 characters long")
+    .max(20, "Adress cannot be longer than 15 characters"),
+  phone: Yup.string()
+    .required("Required")
+    .max(20, "Must be 11 characters or less"),
+  amount: Yup.number()
+    .required("Enter the amount")
+    .positive("Enter a positive amount"),
+  destinationAccount: Yup.string().required("Choose an account"),
+  transactionDate: Yup.string(),
+  transactionTime: Yup.string(),
+  description: Yup.string(),
 });
 
 export default validationSchema;
