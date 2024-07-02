ca8a3b30c295399e357b2b1a7821c832d17aaad2
William <victorwccv@gmail.com>
2024-07-01 20:01:03 -0500

    docs: add comments

--- client/src/components/transactions.components/Deposit.jsx
+++ client/src/components/transactions.components/Deposit.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 import TotalUserBalance from "../TotalUserBalance.jsx";
 import { useData } from "../../hooks/useData.js";
 import Alert from "../Alert.jsx";
-import FormField from "./FormField.jsx";
+import FormField from "../FormField.jsx";
 import validationSchema from "../../validation/validationSchema.js";
 
 const schema = validationSchema.pick([
@@ -17,14 +17,21 @@ const schema = validationSchema.pick([
   "description",
 ]);
 
+/**
+ * Deposit component for depositing funds into a bank account
+ * @returns {JSX.Element} Deposit component
+ */
 function Deposit() {
   const { data } = useData();
   const [error, setError] = useState(null);
   const [showAlert, setShowAlert] = useState(false);
-
   const dispatch = useDispatch();
 
-  const handleSubmit = async (values, setSubmitting) => {
+  /**
+   * Handles form submission for deposit
+   * @param {Object} values - form values
+   */
+  const handleSubmit = async (values) => {
     try {
       setError(null);
       const apiUrl = import.meta.env.VITE_API_URL;
@@ -38,28 +45,28 @@ function Deposit() {
         credentials: "include",
       };
       const response = await fetch(link, options);
-      const data = await response.json();
-      setSubmitting(false);
       if (!response.ok) {
-        setError(data?.message || "An error occured. Please try again later.");
+        const error = await response.json();
+        setError(error?.message || "An error occured. Please try again later.");
         setShowAlert(true);
         return;
       }
+      const data = await response.json();
       setShowAlert(true);
       dispatch(fetchSucces(data));
     } catch (error) {
-      setSubmitting(false);
       setError("An error occured. Please try again later.");
       setShowAlert(true);
     }
   };
 
-  console.log("rendering deposit");
-
+  // Render Deposit component
   return (
     <div className="relative">
       <h2 className="text-3xl text-center font-bold mb-10">Deposit</h2>
+      {/* Render total user balance */}
       <TotalUserBalance data={data} />
+      {/* Render formik form for deposit */}
       <Formik
         initialValues={{
           amount: "",
@@ -69,9 +76,9 @@ function Deposit() {
           description: "",
         }}
         validationSchema={schema}
-        onSubmit={(values, { setSubmitting }) =>
-          handleSubmit(values, setSubmitting)
-        }
+        onSubmit={(values) => {
+          handleSubmit(values);
+        }}
       >
         {(formik) => (
           <Form>
@@ -80,8 +87,10 @@ function Deposit() {
                 formik.isSubmitting ? "opacity-50" : ""
               }`}
             >
+              {/* Render form fields for deposit */}
               <FormField
                 formik={formik}
+                disabled={formik.isSubmitting}
                 type="number"
                 name="amount"
                 label="Amount to Deposit:"
@@ -90,12 +99,14 @@ function Deposit() {
 
               <FormField
                 formik={formik}
+                disabled={formik.isSubmitting}
                 as="select"
                 name="destinationAccount"
                 label="Destination Account:"
                 children={
                   <>
                     <option value="">Select an Account</option>
+                    {/* Render options for destination account */}
                     {data.bankAccounts?.map((acc, index) => {
                       return (
                         <option key={index} value={acc.bankAccountNumber}>
@@ -109,6 +120,7 @@ function Deposit() {
 
               <FormField
                 formik={formik}
+                disabled={formik.isSubmitting}
                 type="text"
                 name="transactionDate"
                 label="Deposit Date:"
@@ -118,14 +130,15 @@ function Deposit() {
 
               <FormField
                 formik={formik}
+                disabled={formik.isSubmitting}
                 type="text"
                 name="description"
                 label="Description or Note:"
               />
-
             </div>
 
-            <div className="flex justify-center mt-12">
+            <div className="flex justify-center mt-8">
+              {/* Render submit button */}
               <button
                 type="submit"
                 disabled={formik.isSubmitting}
@@ -135,10 +148,11 @@ function Deposit() {
               </button>
             </div>
 
+            {/* Render alert if showAlert is true */}
             {showAlert && (
               <Alert
                 success={error === null}
-                message={error === null ? "Deposit successful" : error}
+                message={error === null ? "Successful deposit!" : error}
                 close={
                   error === null
                     ? () => {
@@ -146,7 +160,11 @@ function Deposit() {
                         setError(null);
                         formik.resetForm();
                       }
-                    : () => setShowAlert(false)
+                    : () => {
+                        setShowAlert(false);
+                        setError(null);
+                        formik.setSubmitting(false);
+                      }
                 }
               />
             )}
