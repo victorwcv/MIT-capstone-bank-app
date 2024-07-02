b0297f6a35c5c07778fb61edafc93be440e0d2c4
William <victorwccv@gmail.com>
2024-07-01 19:58:54 -0500

    refactor: use new component for Form Fields

--- client/src/components/transactions.components/BankTransfer.jsx
+++ client/src/components/transactions.components/BankTransfer.jsx
@@ -1,18 +1,29 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form, Field } from "formik";
 import { useData } from "../../hooks/useData";
-import * as Yup from "yup";
 import getCurrentDateTime from "../../utils/dates";
-import {
-  fetchSucces,
-} from "../../store/slices/userDataSlice.js";
+import { fetchSucces } from "../../store/slices/userDataSlice.js";
 import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import TotalUserBalance from "../TotalUserBalance.jsx";
+import Alert from "../Alert.jsx";
+import FormField from "../FormField.jsx";
+import validationSchema from "../../validation/validationSchema.js";
+
+const schema = validationSchema.pick([
+  "amount",
+  "originAccount",
+  "destinationAccount",
+  "transactionDate",
+  "transactionTime",
+  "description",
+  "typeAccount",
+]);
 
 function BankTransfer() {
-  const { data, loading } = useData();
+  const { data } = useData();
+  const [error, setError] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
   const dispatch = useDispatch();
-  const [transactionError, setTransactionError] = useState(null);
   const [accountBalances, setAccountBalances] = useState({});
 
   useEffect(() => {
@@ -23,77 +34,79 @@ function BankTransfer() {
     setAccountBalances(balances);
   }, [data]);
 
-  console.log("Rendering Bank Transfer!");
+  const handleSubmit = async (values) => {
+    try {
+      setError(null);
+      const apiUrl = import.meta.env.VITE_API_URL;
+      const link = `${apiUrl}/api/transaction/bank-transfer`;
+      const options = {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+        credentials: "include",
+      };
+      const response = await fetch(link, options);
+      if (!response.ok) {
+        const error = await response.json();
+        setError(error?.message || "An error occured. Please try again later.");
+        setShowAlert(true);
+        return;
+      }
+      const data = await response.json();
+      dispatch(fetchSucces(data));
+      setShowAlert(true);
+    } catch (error) {
+      setError("An error occured. Please try again later.");
+      setShowAlert(true);
+    }
+  };
+
   return (
-    <>
+    <div className="relative">
       <h2 className="text-3xl text-center font-bold mb-10">Bank transfer</h2>
       <TotalUserBalance data={data} />
-      <div>
-        <Formik
-          initialValues={{
-            originAccount: "",
-            destinationAccount: "",
-            typeAccount: "own",
-            amount: "",
-            transactionDate: getCurrentDateTime().onlyDate,
-            transactionTime: getCurrentDateTime().timeOnly,
-            description: "",
-          }}
-          validate={(values) => {
-            const errors = {};
-            const originAccountBalance =
-              accountBalances[values.originAccount] || 0;
-            if (
-              values.originAccount === values.destinationAccount &&
-              values.originAccount
-            ) {
-              errors.destinationAccount = "Cannot transfer to same account";
-            }
-            if (values.amount && values.amount > originAccountBalance) {
-              errors.amount = `Insufficient funds`;
-            }
 
-            return errors;
-          }}
-          validationSchema={Yup.object({
-            originAccount: Yup.string().required("Required"),
-            destinationAccount: Yup.string().required("Required"),
-            typeAccount: Yup.string().required("Required"),
-            amount: Yup.number()
-              .required("Required")
-              .positive("Cannot be negative"),
-          })}
-          onSubmit={async (values, { resetForm }) => {
-            try {
-              dispatch(fetchStart());
-              const apiUrl = import.meta.env.VITE_API_URL;
-              const link = `${apiUrl}/api/transaction/bank-transfer`;
-              const options = {
-                method: "PATCH",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(values),
-                credentials: "include",
-              };
-              const response = await fetch(link, options);
-              const data = await response.json();
-              console.log(data);
-              if (data.success === false) {
-                dispatch(fetchFailure(data));
-                setTransactionError(data);
-                return;
-              }
-              dispatch(fetchSucces(data));
-            } catch (error) {
-              setTransactionError(error);
-              dispatch(fetchFailure(error));
-            }
-          }}
-        >
-          {(formik) => (
-            <Form>
-              <div className="flex justify-around my-6 text-lg">
+      <Formik
+        initialValues={{
+          originAccount: "",
+          destinationAccount: "",
+          typeAccount: "own",
+          amount: "",
+          transactionDate: getCurrentDateTime().onlyDate,
+          transactionTime: getCurrentDateTime().timeOnly,
+          description: "",
+        }}
+        validate={(values) => {
+          const errors = {};
+          const originAccountBalance =
+            accountBalances[values.originAccount] || 0;
+          if (
+            values.originAccount === values.destinationAccount &&
+            values.originAccount
+          ) {
+            errors.destinationAccount = "Cannot transfer to same account";
+          }
+          if (values.amount && values.amount > originAccountBalance) {
+            errors.amount = `Insufficient funds`;
+          }
+
+          return errors;
+        }}
+        validationSchema={schema}
+        onSubmit={(values) => {
+          handleSubmit(values);
+        }}
+      >
+        {(formik) => (
+          <Form>
+            <div
+              className={`flex items-center overflow-auto justify-center gap-x-10 flex-wrap max-h-[450px] ${
+                formik.isSubmitting ? "opacity-50" : ""
+              }`}
+            >
+              <div className="flex justify-around my-4 w-full">
                 <div className="flex gap-4">
                   <Field type="radio" name="typeAccount" id="own" value="own" />
                   <label htmlFor="own">To own Account</label>
@@ -108,15 +121,13 @@ function BankTransfer() {
                   <label htmlFor="other">To other Account</label>
                 </div>
               </div>
-              <div className="flex  gap-12">
-                <div className="flex flex-col gap-2 text-lg flex-1">
-                  <label htmlFor="originAccount">Choose Origin Account</label>
-                  <Field
-                    as="select"
-                    id="originAccount"
-                    name="originAccount"
-                    className="px-4 py-1 outline-none text-right"
-                  >
+              <FormField
+                formik={formik}
+                as="select"
+                name="originAccount"
+                label="Origin account number"
+                children={
+                  <>
                     <option value="">Select an Account</option>
                     {data.bankAccounts?.map((acc, index) => {
                       return (
@@ -125,114 +136,97 @@ function BankTransfer() {
                         </option>
                       );
                     })}
-                  </Field>
+                  </>
+                }
+              />
 
-                  <div className="text-red-500 text-sm h-3 text-end">
-                    <ErrorMessage name="originAccount" />
-                  </div>
-                </div>
-                <div className="flex flex-col gap-2 text-lg flex-1">
-                  {formik.values.typeAccount === "own" ? (
+              {formik.values.typeAccount === "own" ? (
+                <FormField
+                  formik={formik}
+                  as="select"
+                  name="destinationAccount"
+                  label="Destination account number"
+                  children={
                     <>
-                      <label htmlFor="destinationAccount">
-                        Choose Destination Account
-                      </label>
-                      <Field
-                        as="select"
-                        id="destinationAccount"
-                        name="destinationAccount"
-                        className="px-4 py-1 outline-none text-right"
-                      >
-                        <option>Select an Account</option>
-                        {data.bankAccounts?.map((acc, index) => {
-                          return (
-                            <option key={index} value={acc.bankAccountNumber}>
-                              {`$ ${acc.accountBalance} / ${acc.bankAccountNumber}`}
-                            </option>
-                          );
-                        })}
-                      </Field>
+                      <option>Select an Account</option>
+                      {data.bankAccounts?.map((acc, index) => {
+                        return (
+                          <option key={index} value={acc.bankAccountNumber}>
+                            {`$ ${acc.accountBalance} / ${acc.bankAccountNumber}`}
+                          </option>
+                        );
+                      })}
                     </>
-                  ) : (
-                    <>
-                      <label htmlFor="destinationAccount">
-                        Enter the destination account number
-                      </label>
-                      <Field
-                        type="text"
-                        id="destinationAccount"
-                        name="destinationAccount"
-                        className="px-4 py-1 outline-none text-right"
-                      />
-                    </>
-                  )}
-                  <div className="text-red-500  text-sm h-3 text-end">
-                    <ErrorMessage name="destinationAccount" />
-                  </div>
-                </div>
-              </div>
-              <div className="flex gap-12">
-                <div className="flex flex-col  text-lg flex-1">
-                  <label htmlFor="amount" className="my-2">
-                    Transfer Amount
-                  </label>
-                  <Field
-                    type="number"
-                    id="amount"
-                    name="amount"
-                    className="px-4 py-1 outline-none text-right"
-                  />
-                  <div className="text-red-500 text-sm h-3">
-                    <ErrorMessage name="amount" />
-                  </div>
-                </div>
-                <div className="flex flex-col text-lg flex-1">
-                  <label htmlFor="transactionDate" className="my-2">
-                    Transfer Date
-                  </label>
-                  <Field
-                    type="text"
-                    id="transactionDate"
-                    name="transactionDate"
-                    className="px-4 py-1 outline-none text-right"
-                    readOnly
-                  />
-                  <div className="text-red-500 text-sm h-3 text-end">
-                    <ErrorMessage name="transactionDate" />
-                  </div>
-                </div>
-              </div>
-              <div className="flex flex-col text-lg">
-                <label htmlFor="description" className="my-2">
-                  Description or Note
-                </label>
-                <Field
-                  as="textarea"
-                  id="description"
-                  name="description"
-                  className="w-full h-20 px-4 py-1 outline-none rounded-lg resize-none"
+                  }
                 />
-                <div className="text-red-500 text-sm h-3 text-end">
-                  <ErrorMessage name="description" />
-                </div>
-              </div>
-              <div className="flex justify-center">
-                <button
-                  type="submit"
-                  disabled={loading}
-                  className="btn-primary"
-                >
-                  Complete Transfer
-                </button>
-                <p className="text-red-500 text-right mt-4">
-                  {transactionError && transactionError.message}
-                </p>
-              </div>
-            </Form>
-          )}
-        </Formik>
-      </div>
-    </>
+              ) : (
+                <FormField
+                  formik={formik}
+                  type={"text"}
+                  name={"destinationAccount"}
+                  label={"Destination account number"}
+                />
+              )}
+
+              <FormField
+                formik={formik}
+                type={"number"}
+                name={"amount"}
+                label={"Transfer Amount:"}
+                textRight
+              />
+
+              <FormField
+                formik={formik}
+                type={"text"}
+                name={"transactionDate"}
+                label={"Transfer Date:"}
+                textRight
+                readOnly
+              />
+
+              <FormField
+                formik={formik}
+                type={"text"}
+                name={"description"}
+                label={"Description or Note:"}
+                textRight
+              />
+            </div>
+
+            <div className="flex justify-center mt-8">
+              <button
+                type="submit"
+                disabled={formik.isSubmitting}
+                className="btn-primary"
+              >
+                {formik.isSubmitting ? "Loading..." : "Complete transfer"}
+              </button>
+            </div>
+            {/* Render alert if showAlert is true */}
+            {showAlert && (
+              <Alert
+                success={error === null}
+                message={error === null ? "Successful transfer!" : error}
+                close={
+                  error === null
+                    ? () => {
+                        setShowAlert(false);
+                        setError(null);
+                        formik.resetForm();
+                      }
+                    : () => {
+                        setShowAlert(false);
+                        setError(null);
+                        formik.setSubmitting(false);
+                      }
+                }
+              />
+            )}
+          </Form>
+        )}
+      </Formik>
+    </div>
   );
 }
 
