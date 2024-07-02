b26dcdeaf3b2ba4d723e32069c711a13fc63ff54
William <victorwccv@gmail.com>
2024-07-01 20:03:42 -0500

    refactor: use new component for Form Fields

--- client/src/components/transactions.components/PayBills.jsx
+++ client/src/components/transactions.components/PayBills.jsx
@@ -1,20 +1,62 @@
 import TotalUserBalance from "../TotalUserBalance";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik, Form } from "formik";
 import * as Yup from "yup";
-import AccountsField from "../AccountsField";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { fetchSucces } from "../../store/slices/userDataSlice";
 import getCurrentDateTime from "../../utils/dates";
 import { useData } from "../../hooks/useData";
+import Alert from "../Alert.jsx";
+import FormField from "../FormField.jsx";
+import validationSchema from "../../validation/validationSchema.js";
+
+const schema = validationSchema.pick([
+  "provider",
+  "originAccount",
+  "amount",
+  "transactionDate",
+  "transactionTime",
+  "invoiceNumber",
+  "description",
+]);
 
 function PayBills() {
   const { data } = useData();
-  const [errorPayBills, setErrorPayBills] = useState(null);
+  const [error, setError] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
   const dispatch = useDispatch();
 
+  const handleSubmit = async (values) => {
+    try {
+      setError(null);
+      const apiUrl = import.meta.env.VITE_API_URL;
+      const link = `${apiUrl}/api/transaction/pay-bill`;
+      const options = {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+        credentials: "include",
+      };
+      const response = await fetch(link, options);
+      if (!response.ok) {
+        const error = await response.json();
+        setError(error?.message || "An error occured. Please try again later.");
+        setShowAlert(true);
+        return;
+      }
+      const data = await response.json();
+      setShowAlert(true);
+      dispatch(fetchSucces(data));
+    } catch (error) {
+      setError("An error occured. Please try again later.");
+      setShowAlert(true);
+    }
+  };
+
   return (
-    <>
+    <div className="relative">
       <h2 className="text-3xl text-center font-bold mb-10">Pay Bills</h2>
       <TotalUserBalance data={data} />
       <Formik
@@ -27,145 +69,117 @@ function PayBills() {
           invoiceNumber: "",
           description: "",
         }}
-        validationSchema={Yup.object({
-          provider: Yup.string().required("Required field"),
-          amount: Yup.number().positive().required("Enter the amount"),
-          originAccount: Yup.string().required("Required field"),
-          invoiceNumber: Yup.string().required("Required field"),
-          description: Yup.string(),
-        })}
-        onSubmit={async (values, { resetForm }) => {
-          try {
-            const apiUrl = import.meta.env.VITE_API_URL;
-            const link = `${apiUrl}/api/transaction/pay-bills/`;
-            const options = {
-              method: "PATCH",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify(values),
-              credentials: "include",
-            };
-            const response = await fetch(link, options);
-            const data = await response.json();
-            if (data.success === false) {
-              setErrorPayBills(data);
-              return;
-            }
-            setErrorPayBills(null);
-            dispatch(fetchSucces(data));
-            resetForm();
-          } catch (error) {
-            setErrorPayBills(error);
-            console.log(error);
-          }
+        validationSchema={schema}
+        onSubmit={(values) => {
+          handleSubmit(values);
         }}
       >
         {(formik) => (
           <Form>
-            <div className="flex items-center justify-center gap-x-10 flex-wrap max-h-[450px]">
-              <div className="relative flex flex-1 flex-col pb-6  min-w-80">
-                <label htmlFor="provider">Provider:</label>
-                <Field
-                  as="select"
-                  id="provider"
-                  name="provider"
-                  className={`${
-                    formik.errors.provider && formik.touched.provider
-                      ? " border-2 border-red-500"
-                      : ""
-                  }`}
-                >
-                  <option value="">Select a provider</option>
-                  <option value="electricity">Electricity</option>
-                  <option value="water">Water</option>
-                  <option value="gas">Gas</option>
-                  <option value="internet">Internet</option>
-                  <option value="phone">Phone</option>
-                  <option value="cable">Cable</option>
-                  <option value="tv">TV</option>
-                  <option value="other">Other</option>
-                </Field>
-                <ErrorMessage
-                  name="provider"
-                  component="div"
-                  className="absolute bottom-0 right-2 text-red-500"
-                />
-              </div>
-              <div className="relative flex flex-1 flex-col pb-6  min-w-80">
-                <label htmlFor="invoiceNumber">Invoce Number:</label>
-                <Field
-                  type="text"
-                  name="invoiceNumber"
-                  id="invoiceNumber"
-                  className={`${
-                    formik.errors.invoiceNumber && formik.touched.invoiceNumber
-                      ? " border-2 border-red-500"
-                      : ""
-                  }`}
-                />
-                <ErrorMessage
-                  name="invoiceNumber"
-                  component="div"
-                  className="absolute bottom-0 right-2 text-red-500"
-                />
-              </div>
+            <div
+              className={`flex items-center justify-center gap-x-10 flex-wrap max-h-[450px] ${
+                formik.isSubmitting ? "opacity-50" : ""
+              }`}
+            >
+              <FormField
+                formik={formik}
+                as={"select"}
+                name={"provider"}
+                label={"Provider:"}
+                children={
+                  <>
+                    <option value="">Select a provider</option>
+                    <option value="electricity">Electricity</option>
+                    <option value="water">Water</option>
+                    <option value="gas">Gas</option>
+                    <option value="internet">Internet</option>
+                    <option value="phone">Phone</option>
+                    <option value="cable">Cable</option>
+                    <option value="tv">TV</option>
+                    <option value="other">Other</option>
+                  </>
+                }
+              />
 
-              <div className="relative flex flex-1 flex-col pb-6  min-w-80">
-                <label htmlFor="amount">Amount:</label>
-                <Field
-                  type="text"
-                  name="amount"
-                  id="amount"
-                  className={`${
-                    formik.errors.amount && formik.touched.amount
-                      ? " border-2 border-red-500"
-                      : ""
-                  }`}
-                />
-                <ErrorMessage
-                  name="amount"
-                  component="div"
-                  className="absolute bottom-0 right-2 text-red-500"
-                />
-              </div>
-              <div className="relative flex flex-1 flex-col pb-6  min-w-80">
-                <label htmlFor="originAccount">Account for payment:</label>
-                <AccountsField name={"originAccount"} formik={formik} />
-                <ErrorMessage
-                  name="originAccount"
-                  component="div"
-                  className="absolute bottom-0 right-2 text-red-500"
-                />
-              </div>
-              <div className="relative flex flex-1 flex-col pb-6 min-w-80">
-                <label htmlFor="description">Description:</label>
-                <Field
-                  as="textarea"
-                  name="description"
-                  id="description"
-                  className="h-20 px-4 py-1 outline-none resize-none"
-                />
-                <ErrorMessage
-                  name="description"
-                  component="div"
-                  className="absolute bottom-0 right-2 text-red-500"
-                />
-              </div>
+              <FormField
+                formik={formik}
+                disabled={formik.isSubmitting}
+                type={"text"}
+                name={"invoiceNumber"}
+                label={"Invoce Number:"}
+              />
+
+              <FormField
+                formik={formik}
+                disabled={formik.isSubmitting}
+                type={"number"}
+                name={"amount"}
+                label={"Amount to pay:"}
+              />
+
+              <FormField
+                formik={formik}
+                disabled={formik.isSubmitting}
+                as="select"
+                name="originAccount"
+                label="Account for payment:"
+                children={
+                  <>
+                    <option value="">Select an Account</option>
+                    {/* Render options for destination account */}
+                    {data.bankAccounts?.map((acc, index) => {
+                      return (
+                        <option key={index} value={acc.bankAccountNumber}>
+                          {`$ ${acc.accountBalance} / ${acc.bankAccountNumber}`}
+                        </option>
+                      );
+                    })}
+                  </>
+                }
+              />
+
+              <FormField
+                formik={formik}
+                disabled={formik.isSubmitting}
+                type="text"
+                name="description"
+                label="Description or Note:"
+              />
             </div>
-            <div className="flex justify-center mt-2">
+            <div className="flex justify-center mt-8">
+              {/* Render submit button */}
               <button
                 type="submit"
                 disabled={formik.isSubmitting}
                 className="btn-primary"
               >
-                Complete Payment
+                {formik.isSubmitting ? "Loading..." : "Complete Payment"}
               </button>
             </div>
+
+            {showAlert && (
+              <Alert
+                success={error === null}
+                message={error === null ? "Successful payment!" : error}
+                close={
+                  error === null
+                    ? () => {
+                        setShowAlert(false);
+                        setError(null);
+                        formik.resetForm();
+                      }
+                    : () => {
+                        setShowAlert(false);
+                        setError(null);
+                        formik.setSubmitting(false);
+                      }
+                }
+              />
+            )}
           </Form>
         )}
       </Formik>
-    </>
+    </div>
   );
 }
 
