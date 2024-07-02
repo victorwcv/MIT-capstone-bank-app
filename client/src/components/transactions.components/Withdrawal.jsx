4d4473f22b3e8490227bf7edc091c9379414f33d
William <victorwccv@gmail.com>
2024-07-01 20:08:29 -0500

    refactor: use new component for Form Fields

--- client/src/components/transactions.components/Withdrawal.jsx
+++ client/src/components/transactions.components/Withdrawal.jsx
@@ -2,22 +2,29 @@ import { Formik, Form, Field, ErrorMessage } from "formik";
 import getCurrentDateTime from "../../utils/dates.js";
 import * as Yup from "yup";
 import { useDispatch, useSelector } from "react-redux";
-import {
-  fetchSucces,
-} from "../../store/slices/userDataSlice.js";
+import { fetchSucces } from "../../store/slices/userDataSlice.js";
 import { useState } from "react";
 import TotalUserBalance from "../TotalUserBalance.jsx";
+import Alert from "../Alert.jsx";
+import FormField from "../FormField.jsx";
+import validationSchema from "../../validation/validationSchema.js";
+
+const schema = validationSchema.pick([
+  "amount",
+  "originAccount",
+  "transactionDate",
+  "transactionTime",
+  "description",
+]);
 
 function Withdrawal() {
-  const { loading, data, error } = useSelector((state) => state.userData);
-  const [transactionError, setTransactionError] = useState(null);
-
-  console.log("rendering withdrawal");
+  const { data } = useSelector((state) => state.userData);
+  const [error, setError] = useState(null);
+  const [showAlert, setShowAlert] = useState(false);
   const dispatch = useDispatch();
 
-  const handleSubmit = async (values, reset) => {
+  const handleSubmit = async (values) => {
     try {
-      dispatch(fetchStart());
       const apiUrl = import.meta.env.VITE_API_URL;
       const link = `${apiUrl}/api/transaction/withdrawal`;
       const options = {
@@ -29,22 +36,23 @@ function Withdrawal() {
         credentials: "include",
       };
       const response = await fetch(link, options);
-      const data = await response.json();
-      if (data.success === false) {
-        dispatch(fetchFailure(data));
-        setTransactionError(data);
+      if (!response.ok) {
+        const error = await response.json();
+        setError(error?.message || "An error occured. Please try again later.");
+        setShowAlert(true);
         return;
       }
+      const data = await response.json();
       dispatch(fetchSucces(data));
-      reset();
+      setShowAlert(true);
     } catch (error) {
-      dispatch(fetchFailure(error));
-      setTransactionError(error);
+      setError("An error occured. Please try again later.");
+      setShowAlert(true);
     }
   };
 
   return (
-    <>
+    <div className="relative">
       <h2 className="text-3xl text-center font-bold mb-10">Withdrawal</h2>
       <TotalUserBalance data={data} />
       <Formik
@@ -55,108 +63,97 @@ function Withdrawal() {
           transactionTime: getCurrentDateTime().timeOnly,
           description: "",
         }}
-        validationSchema={Yup.object({
-          amount: Yup.number()
-            .positive("Enter a positive amount")
-            .required("Required"),
-          originAccount: Yup.string().required("Required"),
-          transactionDate: Yup.string(),
-          transactionTime: Yup.string(),
-          description: Yup.string(),
-        })}
-        onSubmit={async (values, { resetForm }) => {
-          handleSubmit(values, resetForm);
-          console.log(values);
+        validationSchema={schema}
+        onSubmit={(values) => {
+          handleSubmit(values);
         }}
       >
         {(formik) => (
-          <Form className="grid grid-cols-2 gap-20">
-            <div>
-              <div className="flex flex-col my-4 text-xl">
-                <label htmlFor="amount" className="my-2">
-                  Amount to Withdraw
-                </label>
-                <Field
-                  type="number"
-                  id="amount"
-                  name="amount"
-                  className="px-4 py-1 outline-none text-right"
-                />
-                <div className="text-red-500 mt-1 text-sm font-light h-3">
-                  <ErrorMessage name="amount" />
-                </div>
-              </div>
+          <Form>
+            <div
+              className={`flex items-center justify-center gap-x-10 flex-wrap max-h-[450px] ${
+                formik.isSubmitting ? "opacity-50" : ""
+              }`}
+            >
+              <FormField
+                formik={formik}
+                disabled={formik.isSubmitting}
+                type="number"
+                name="amount"
+                label="Amount to Withdraw:"
+                textRight
+              />
 
-              <div className="flex flex-col my-4 text-xl">
-                <label htmlFor="originAccount" className="my-2">
-                  Origin Account
-                </label>
+              <FormField
+                formik={formik}
+                disabled={formik.isSubmitting}
+                as="select"
+                name="originAccount"
+                label="Origin Account:"
+                children={
+                  <>
+                    <option value="">Select an Account</option>
+                    {data.bankAccounts?.map((acc, index) => {
+                      return (
+                        <option key={index} value={acc.bankAccountNumber}>
+                          {`$ ${acc.accountBalance} / ${acc.bankAccountNumber}`}
+                        </option>
+                      );
+                    })}
+                  </>
+                }
+              />
 
-                <Field
-                  as="select"
-                  id="originAccount"
-                  name="originAccount"
-                  className="px-4 py-1 outline-none text-right"
-                >
-                  <option value="">Select an Account</option>
-                  {data.bankAccounts?.map((acc, index) => {
-                    return (
-                      <option key={index} value={acc.bankAccountNumber}>
-                        {`$ ${acc.accountBalance} / ${acc.bankAccountNumber}`}
-                      </option>
-                    );
-                  })}
-                </Field>
+              <FormField
+                formik={formik}
+                disabled={formik.isSubmitting}
+                type="text"
+                name="transactionDate"
+                label="Deposit Date:"
+                textRight
+                readOnly
+              />
 
-                <div className="text-red-500 mt-1 text-sm font-light h-3">
-                  <ErrorMessage name="originAccount" />
-                </div>
-              </div>
-
-              <div className="flex flex-col my-4 text-xl">
-                <label htmlFor="transactionDate" className="my-2">
-                  Withdrawal Date
-                </label>
-                <Field
-                  type="text"
-                  id="transactionDate"
-                  name="transactionDate"
-                  className="px-4 py-1 outline-none text-right"
-                  readOnly
-                />
-                <div className="text-red-500 mt-1 text-sm font-light h-3">
-                  <ErrorMessage name="transactionDate" />
-                </div>
-              </div>
+              <FormField
+                formik={formik}
+                disabled={formik.isSubmitting}
+                type="text"
+                name="description"
+                label="Description or Note:"
+              />
             </div>
-            <div className="flex flex-col ">
-              <div className="flex flex-col my-4 text-xl">
-                <label htmlFor="description" className="my-2">
-                  Description or Note
-                </label>
-                <Field
-                  as="textarea"
-                  id="description"
-                  name="description"
-                  className="w-full h-32 px-4 py-1 outline-none rounded-lg resize-none"
-                />
-                <ErrorMessage name="description" />
-              </div>
+            <div className="flex justify-center mt-8">
               <button
                 type="submit"
                 disabled={formik.isSubmitting}
                 className="btn-primary"
               >
-                {loading ? "Loading..." : "Withdraw"}
+                {formik.isSubmitting ? "Loading..." : "Complete Withdrawal"}
               </button>
-              <p className="text-red-500 text-right mt-4">
-                {transactionError && transactionError.message}
-              </p>
             </div>
+            {showAlert && (
+              <Alert
+                success={error === null}
+                message={error === null ? "Successful withdrawal!" : error}
+                close={
+                  error === null
+                    ? () => {
+                        setShowAlert(false);
+                        setError(null);
+                        formik.resetForm();
+                      }
+                    : () => {
+                        setShowAlert(false);
+                        setError(null);
+                        formik.setSubmitting(false);
+                      }
+                }
+              />
+            )}
           </Form>
         )}
       </Formik>
-    </>
+    </div>
   );
 }
 
