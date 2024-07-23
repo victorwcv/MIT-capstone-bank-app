460cd072b132fca6aa4e68e2256edfbf4c5784d4
William <victorwccv@gmail.com>
2024-07-23 18:00:23 -0500

    fix: :sparkles: shows footer only in  home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/navbar.component/Navbar";
 import Footer from "./components/footer.component/Footer";
 import Disclaimer from "./components/Disclaimer";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="relative">
       <Navbar />
       <Outlet />
-      <Footer />
+      {location.pathname === "/" && <Footer />}
       <Disclaimer />
     </div>
   );
