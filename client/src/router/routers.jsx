a2036fe1bef64d067521258b057d46cb6c6aa6c1
William <victorwccv@gmail.com>
2025-08-16 00:16:19 -0500

    fix: update router

--- client/src/router/routers.jsx
+++ client/src/router/routers.jsx
@@ -27,127 +27,132 @@ import SearchUser from "../components/admin_panel.components/SearchUser.jsx";
 import AdminLog from "../components/admin_panel.components/AdminLog.jsx";
 import DeleteUser from "../components/admin_panel.components/DeleteUser.jsx";
 
-const Router = createBrowserRouter([
+const Router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "",
+          element: <Home />,
+        },
+        {
+          // Public Routes
+          path: "",
+          element: <PublicRoute />,
+          children: [
+            {
+              path: "create-account",
+              element: <CreateAccount />,
+            },
+            {
+              path: "online-banking",
+              element: <OnlineBanking />,
+            },
+          ],
+        },
+
+        // Admin Routes
+        {
+          path: "admin-panel",
+          element: <AdminRoute />,
+          children: [
+            {
+              path: "",
+              element: <AdminPanel />,
+              children: [
+                {
+                  path: "",
+                  element: <AdminData />,
+                },
+                {
+                  path: "",
+                  element: <OperateAdminRoute />,
+                  children: [
+                    {
+                      path: "all-data",
+                      element: <AllData />,
+                    },
+                    {
+                      path: "create-new-admin",
+                      element: <CreateNewAdmin />,
+                    },
+                    {
+                      path: "search-user",
+                      element: <SearchUser />,
+                    },
+                    {
+                      path: "admin-log",
+                      element: <AdminLog />,
+                    },
+                    {
+                      path: "delete-user",
+                      element: <DeleteUser />,
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+
+        // Dashboard Routes
+        {
+          path: "dashboard",
+          element: <PrivateRoute />,
+          children: [
+            {
+              path: "",
+              element: <Dashboard />,
+            },
+          ],
+        },
+
+        // Transaction Routes
+        {
+          path: "transactions",
+          element: <PrivateRoute />,
+          children: [
+            {
+              path: "",
+              element: <Transactions />,
+              children: [
+                {
+                  path: "deposit",
+                  element: <Deposit />,
+                },
+                {
+                  path: "withdrawal",
+                  element: <Withdrawal />,
+                },
+                {
+                  path: "new-bank-account",
+                  element: <NewBankAccount />,
+                },
+                {
+                  path: "bank-transfer",
+                  element: <BankTransfer />,
+                },
+                {
+                  path: "pay-bills",
+                  element: <PayBills />,
+                },
+                {
+                  path: "delete-bank-account",
+                  element: <CloseAccount />,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "",
-        element: <Home />,
-      },
-      {
-        // Public Routes
-        path: "",
-        element: <PublicRoute />,
-        children: [
-          {
-            path: "/create-account",
-            element: <CreateAccount />
-          },
-          {
-            path: "/online-banking",
-            element: <OnlineBanking />,
-          },
-        ],
-      },
-
-      // Admin Routes
-      {
-        path: "/admin-panel",
-        element: <AdminRoute />,
-        children: [
-          {
-            path: "",
-            element: <AdminPanel />,
-            children: [
-              {
-                path: "",
-                element: <AdminData />,
-              },
-              {
-                path: "",
-                element: <OperateAdminRoute />,
-                children: [
-                  {
-                    path: "all-data",
-                    element: <AllData />,
-                  },
-                  {
-                    path: "create-new-admin",
-                    element: <CreateNewAdmin />,
-                  },
-                  {
-                    path: "search-user",
-                    element: <SearchUser />,
-                  },
-                  {
-                    path: "admin-log",
-                    element: <AdminLog />,
-                  },
-                  {
-                    path: "delete-user",
-                    element: <DeleteUser />
-                  }
-                ],
-              },
-            ],
-          },
-        ],
-      },
-
-      // Dashboard Routes
-      {
-        path: "/dashboard",
-        element: <PrivateRoute />,
-        children: [
-          {
-            path: "",
-            element: <Dashboard />,
-          },
-        ],
-      },
-
-      // Transaction Routes
-      {
-        path: "/transactions",
-        element: <PrivateRoute />,
-        children: [
-          {
-            path: "",
-            element: <Transactions />,
-            children: [
-              {
-                path: "/transactions/deposit",
-                element: <Deposit />,
-              },
-              {
-                path: "/transactions/withdrawal",
-                element: <Withdrawal />,
-              },
-              {
-                path: "/transactions/new-bank-account",
-                element: <NewBankAccount />,
-              },
-              {
-                path: "/transactions/bank-transfer",
-                element: <BankTransfer />,
-              },
-              {
-                path: "/transactions/pay-bills",
-                element: <PayBills />,
-              },
-              {
-                path: "/transactions/delete-bank-account",
-                element: <CloseAccount />,
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
-]);
+    basename: "/MIT-capstone-bank-app",
+  }
+);
 
 export default Router;
