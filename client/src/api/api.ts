da213ca3ffe57a1ef63cb0244d89ff0fee0e842b
William <victorwccv@gmail.com>
2025-08-28 01:25:02 -0500

    fix: update urls

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:5000/api/v1";
+// const API_BASE_URL = "http://localhost:5000/api/v1";
+const API_BASE_URL = "https://victor-ccanchifullstackbankingapplication.onrender.com10000/api/v1";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/banking-app";
+const MONGO_URI = process.env._MONGO_DB || "mongodb://localhost:27017/banking-app";
 
 export const connectDB = async () => {
   try {
