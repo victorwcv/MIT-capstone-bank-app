b31c77dac6d18f40f62b8da10eb44ab9f8474457
William <victorwccv@gmail.com>
2025-08-28 01:28:09 -0500

    fix:update public url

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const API_BASE_URL = "http://localhost:5000/api/v1";
-const API_BASE_URL = "https://victor-ccanchifullstackbankingapplication.onrender.com10000/api/v1";
+const API_BASE_URL = "https://mit-capstone-bank-app.onrender.com/api/v1";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
